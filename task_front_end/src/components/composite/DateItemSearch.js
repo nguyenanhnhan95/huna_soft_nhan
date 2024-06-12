248dce1b19c80d2babfb7d18fdd06c28d37b750c
nguyenanhnhan95 <nguyenanhnhan95@gmail.com>
2024-06-12 19:17:09 +0700

    12/06

--- task_front_end/src/components/composite/DateItemSearch.js
+++ task_front_end/src/components/composite/DateItemSearch.js
@@ -5,7 +5,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import "../../css/composite/dateItemSearch.css"
-// import required css from library
+// import required css from library asdasdasd
 
 function DateItemSearch(props) {
     const { handleSetQuery, item, query } = props;
